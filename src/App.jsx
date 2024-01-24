2c97f8401b5442634768d9dd6e9dd9ee279132b8
Mateusz Majer <mateusz.majer996@gmail.com>
2024-01-24 23:39:13 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import styles from "./App.module.css";
+import { Form } from "./components/Form/Form";
 
 function App() {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <div>
-          <h1>Do zrobienia</h1>
-          <h2>5 zadań</h2>
+          <h1>TO DO:</h1>
+          <h2>5 task(s)</h2>
         </div>
         <button className={styles.button}>+</button>
       </header>
+      <Form />
     </div>
   );
 }
