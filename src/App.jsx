4da4058eab065fa5c649e8a05e219e0c8f2c2127
Mateusz Majer <mateusz.majer996@gmail.com>
2024-01-27 23:23:58 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
     <div className={styles.container}>
       <header className={styles.header}>
         <div>
-          <h1>TO DO:</h1>
+          <h1>TO DO LIST:</h1>
           <h2>{getSubheading(todos.length)}</h2>
         </div>
         {!isFormShown && (
